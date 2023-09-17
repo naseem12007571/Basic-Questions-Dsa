fb9b77b355bd3d387cd7d2947816e17df9aa17ae
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-17 11:25:17 +0530

    Create Number Greater and less.cpp

---
+++ Number Greater and less.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int a,b;
+    cout<<"Enter the number a "<<endl;
+    cin>>a;
+    cout<<"Enter the number b"<<endl;
+    cin>>b;
+    
+    if(a>b){
+        cout<<"A is greater "<<endl;
+    }
+    else{
+        cout<<"A is less";
+    }
+}
