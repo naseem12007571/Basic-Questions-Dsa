570745de1bb48bd54ecc1a4092440e04f4a81d69
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-21 00:34:52 +0530

    commit

---
+++ im.pattern.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    int row=1;
+    while(row<=n){
+        // space print first triangle
+        int space = n-row;
+        while(space){
+            cout<<" ";
+            space = space-1;
+        }
+        
+        // print second triangle
+        int col=1;
+        while(col<=row){
+            cout<<col;
+            col=col+1;
+        }
+        
+        // print third triangle
+        int start= row-1;
+        while(start){
+            cout<<start;
+            start=start-1;
+        }
+        
+        
+        
+        
+        cout<<endl;
+        row=row+1;
+    }
+    
+}
+
+//     1
+//    121
+//   12321
+//  1234321
+// 123454321
+
