f2ebd891bcae5b16e11dc2f95bcad1d0aa47ab56
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-20 09:58:44 +0530

    commit

---
+++ pattern Character.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int row=1;
+    while(row<=n){
+        int col=1;
+        char ch= 'A'+row-1;
+        while(col<=n){
+            cout<<ch<<" ";
+            col=col+1;
+        }
+        cout<<endl;
+        row=row+1;
+    }
+    
+}
+
+
+4
+A A A A 
+B B B B 
+C C C C 
+D D D D 
