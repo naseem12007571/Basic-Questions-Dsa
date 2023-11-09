67716609d19bf7697cbe01634dcb4f673219178e
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-11-09 23:06:42 +0530

    Create multiple table.cpp

---
+++ multiple table.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   
+   int n;
+   cout<<"Enter the Table name ";
+   cin>>n;
+   for(int i=1;i<=10;i++){
+       cout<<n*i<<" ";
+      
+   }
+   
+    
+}
